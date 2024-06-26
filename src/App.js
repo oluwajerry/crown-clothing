1c149e1f6f10c684073c4639d2a239d9f148d937
oluwajerry <amosjeremiah92@gmail.com>
2024-06-26 21:57:31 +0100

    updating header component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import shopPage from './component/pages/shop/shop.component';
 
 import HomePage from './component/pages/homepage/homepage.component';
+import Header from './component/header/header.component';
 import './App.css';
 
 
@@ -11,6 +12,7 @@ import './App.css';
 function App() {
   return (
     <div >
+      <Header />
       <Routes>
         <Route  path='/' Component={HomePage} />
         <Route  path='/shop' Component={shopPage} />
